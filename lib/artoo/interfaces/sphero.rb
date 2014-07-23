1b000afc5596306609b3741eef5958d85314733f
deadprogram <ron@hybridgroup.com>
2014-07-22 20:02:42 -0700

    WIP on adding Rover interface to Sphero robots

---
+++ lib/artoo/interfaces/sphero.rb
@@ -0,0 +1,28 @@
+require 'artoo/interfaces/interface'
+
+module Artoo
+  module Interfaces
+    # The Sphero Rover interface
+    class Sphero < Rover
+    	def forward(speed)
+        @device.roll 0, speed
+    	end
+
+    	def backward(speed)
+        @device.roll 90, speed
+    	end
+
+    	def turn_left(degrees)
+        # TODO: implement
+    	end
+
+    	def turn_right(degrees)
+        # TODO: implement
+    	end
+
+    	def stop
+        @device.stop
+    	end    	
+    end
+  end
+end

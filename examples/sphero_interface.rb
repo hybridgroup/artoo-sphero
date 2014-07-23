cb36ec72e811c02dc81c54da589077275bbaece5
deadprogram <ron@hybridgroup.com>
2014-07-23 08:19:27 -0700

    Add concept example for rover interface

--- examples/sphero_interface.rb
+++ examples/sphero_interface.rb
@@ -9,9 +9,11 @@ work do
   every(3.seconds) do
     puts "Rolling..."
     unless @rolling
+      # TODO: `forward 90` should be equivilent
       interfaces(:rover).forward 90
       @rolling = true
     else
+      # TODO: `backward 90` should be equivilent
       interfaces(:rover).backward 90
       @rolling = false
     end

82de30f9af4b9504156d44c71e8e710fab343013
deadprogram <ron@hybridgroup.com>
2014-07-23 15:03:03 -0700

    Dynamic methods can be added to robot via device interfaces

--- examples/sphero_interface.rb
+++ examples/sphero_interface.rb
@@ -9,12 +9,12 @@ work do
   every(3.seconds) do
     puts "Rolling..." 
     unless @rolling
-      # TODO: `forward 90` should be equivilent
-      interfaces[:rover].forward(90)
+      # same as interfaces[:rover].forward(90)
+      forward(90)
       @rolling = true
     else
-      # TODO: `backward 90` should be equivilent
-      interfaces[:rover].backward(90)
+      #  same as interfaces[:rover].backward(90)
+      backward(90)
       @rolling = false
     end
   end

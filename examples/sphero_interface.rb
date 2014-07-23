a6ab32fbe31aa386fed2110ea82c6532988ac836
deadprogram <ron@hybridgroup.com>
2014-07-23 14:19:52 -0700

    Correct way to call interface methods

--- examples/sphero_interface.rb
+++ examples/sphero_interface.rb
@@ -7,14 +7,14 @@ work do
   @rolling = false
 
   every(3.seconds) do
-    puts "Rolling..."
+    puts "Rolling..." 
     unless @rolling
       # TODO: `forward 90` should be equivilent
-      interfaces(:rover).forward 90
+      interfaces[:rover].forward(90)
       @rolling = true
     else
       # TODO: `backward 90` should be equivilent
-      interfaces(:rover).backward 90
+      interfaces[:rover].backward(90)
       @rolling = false
     end
   end
--- lib/artoo-sphero.rb
+++ lib/artoo-sphero.rb
@@ -1,3 +1,4 @@
 require 'lib/artoo/adaptors/sphero'
 require 'lib/artoo/drivers/sphero'
+require 'lib/artoo/interfaces/sphero'
 require 'lib/artoo-sphero/version'
